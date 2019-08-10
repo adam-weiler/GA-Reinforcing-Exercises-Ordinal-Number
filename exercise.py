76d7fdc5d32b4b31c82d42595db20e2c9110ea9e
adam-weiler <adam.weiler@yahoo.ca>
2019-08-10 14:43:13 -0400

    Solves any number positive or negative, but there is a lot of repetitive code.

--- exercise.py
+++ exercise.py
@@ -1,10 +1,22 @@
-ordinal_indicator = {1:'st', 2:'nd', 3:'rd', 4:'th', 5:'th', 6:'th', 7:'th', 8:'th', 9:'th', 10:'th', 11:'th', 12:'th', 13:'th', 14:'th', 15:'th', 16:'th', 17:'th', 18:'th', 19:'th', 20:'th', 21:'st'}
+# ordinal_indicator = {0: 'th', 1:'st', 2:'nd', 3:'rd', 4:'th', 5:'th', 6:'th', 7:'th', 8:'th', 9:'th', 10:'th', 11:'th', 12:'th', 13:'th', 14:'th', 15:'th', 16:'th', 17:'th', 18:'th', 19:'th', 20:'th', 21:'st'}
 
+ordinal_indicator = {0: 'th', 1:'st', 2:'nd', 3:'rd', 4:'th', 5:'th', 6:'th', 7:'th', 8:'th', 9:'th', 10:'th'}
 
 def get_ordinal_number(num):
-    return str(num) + ordinal_indicator[num]
+    last_digit = abs(num) % 10  # The abs function will treat all numbers as positive.
+
+    return str(num) + ordinal_indicator[last_digit]
 
 
+print(get_ordinal_number(-205))
+print(get_ordinal_number(-6))
+print(get_ordinal_number(0))
 print(get_ordinal_number(1))
-print(get_ordinal_number(7))
-print(get_ordinal_number(21))
+print(get_ordinal_number(2))
+print(get_ordinal_number(23))
+print(get_ordinal_number(100))
+print(get_ordinal_number(104))
+print(get_ordinal_number(1007))
+print(get_ordinal_number(2118))
+print(get_ordinal_number(3959))
+print(get_ordinal_number(40153160))
